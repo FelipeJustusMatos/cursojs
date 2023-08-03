e325ad434d790e5efba16c9cf10a705ca4f4ed91
FelipeJustusMatos <felipevalitaa@gmail.com>
2023-08-03 10:36:57 -0300

    aula14

---
+++ aula13/ex014.js
@@ -0,0 +1,13 @@
+/*console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+*/
+
+var count = 1;
+while (count <= 6) {
+  console.log(`Passo ${count}`);
+  count++;
+}
