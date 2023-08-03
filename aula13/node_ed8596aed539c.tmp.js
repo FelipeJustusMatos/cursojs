c273cb4c829e2a70ae4241694c4a65c362bd1b53
FelipeJustusMatos <felipevalitaa@gmail.com>
2023-08-03 10:54:59 -0300

    exercicio plus aula 14

--- aula13/node_ed8596aed539c.tmp.js
+++ aula13/node_ed8596aed539c.tmp.js
@@ -7,13 +7,7 @@ console.log("Tudo bem?");
 */
 
 var count = 1;
-do {
+while (count <= 5000000000000000000000) {
   console.log(`Passo ${count}`);
   count++;
-} while (count <= 6);
-
-/* var count = 1;
-while (count <= 6) {
-  console.log(`Passo ${count}`);
-  count++;
-}*/
+}
