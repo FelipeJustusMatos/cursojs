ec6d1c25087a62493709fab214ebc363fb900955
FelipeJustusMatos <felipevalitaa@gmail.com>
2023-08-28 14:43:12 -0300

    aula15-vetor

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3];
+//num[5] = 11;
+//num.push(15);
+//num.length;
+num.sort();
+
+console.log(`Nosso vetor é o ${num}`);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro vetor é ${num[0]}`);
